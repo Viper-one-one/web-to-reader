afd599bdebe0726d6f8c4b2b7164e909ee52a54b
Viper-one-one <taylor.j.nastally@gmail.com>
2025-09-14 20:28:43 -0700

    refactor: streamline form handling by moving onChange to input elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,8 +46,7 @@ export default function Home() {
       <div className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
         <h1 className="text-4xl font-bold">Web to Reader</h1>
         <form className="mt-8 w-full max-w-md border border-blue-400 p-2 rounded-lg shadow-md"
-          onSubmit={handleSubmit}
-          onChange={handleChange}>
+          onSubmit={handleSubmit}>
           <label htmlFor="url" className="block text-sm font-medium text-gray-500">
             Enter URL:
           </label>
@@ -57,6 +56,7 @@ export default function Home() {
             name="url"
             placeholder="https://example.com"
             className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
+            onChange={handleChange}
           />
           <label className="block text-sm font-medium text-gray-500 mt-4">
             Select Format:
@@ -66,13 +66,13 @@ export default function Home() {
               <label className="block text-sm font-medium text-gray-500">
                 PDF
               </label>
-              <input type="radio" name="format" value="PDF" />
+              <input type="radio" name="format" value="PDF" onChange={handleChange} />
             </div>
             <div className="flex flex-col justify-items-evenly">
               <label className="block text-sm font-medium text-gray-500">
                 EPUB
               </label>
-              <input type="radio" name="format" value="EPUB" />
+              <input type="radio" name="format" value="EPUB" onChange={handleChange} />
             </div>
           </div>
             <button
